2ada84e58f8e798ac650d8b5d8f2e07f023748a8
Randy Tate <randytatejr@gmail.com>
2019-09-27 18:18:44 -0500

    added veggie options buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ import bread from "./components/bread.js";
 import meat from "./components/meat.js";
 import cheese from "./components/cheese.js";
 import condiments from "./components/condiments.js";
-
+import veggies from "./components/veggies.js";
 
 // init function always goes in the main.js
 const init = () => {
@@ -10,6 +10,7 @@ const init = () => {
     meat.printMeatOptions();
     cheese.printCheeseOptions();
     condiments.printCondimentOptions();
+    veggies.printVeggieOptions();
 };
 
 init();
